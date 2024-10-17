5dbc0e97720eb8690c23d3ed605a83492d80c722
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-10-17 15:30:01 +0900

    메서드 이름으로 쿼리를 조합하는 쿼리메서드 방식

--- src/main/java/com/study/jpa/chap02/entity/repository/StudentRepository.java
+++ src/main/java/com/study/jpa/chap02/entity/repository/StudentRepository.java
@@ -3,6 +3,24 @@ package com.study.jpa.chap02.entity.repository;
 import com.study.jpa.chap02.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, String> {
+
+    List<Student> findByName(String name);
+
+    List<Student> findByCityAndMajor(String city, String major);
+
+    // WHERE major LIKE '%major%'
+    List<Student> findByMajorContaining(String major);
+
+    // WHERE major LIKE 'major%'
+    List<Student> findByMajorStartingWith(String major);
+
+    // WHERE major LIKE '%major'
+    List<Student> findByMajorEndingWith(String major);
+
+    // WHERE age <= ?
+//    List<Student> findByAgeLessThanEqual(int age);
 
 }
--- src/test/java/com/study/jpa/chap02/entity/repository/StudentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap02/entity/repository/StudentRepositoryTest.java
@@ -1,8 +1,14 @@
 package com.study.jpa.chap02.entity.repository;
 
+import com.study.jpa.chap02.entity.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +17,60 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentRepositoryTest {
 
     @Autowired
-    StudentRepository studentRepository
+    StudentRepository studentRepository;
+
+    @BeforeEach
+    void insertData() {
+        Student s1 = Student.builder()
+                .name("쿠로미")
+                .city("청양군")
+                .major("경제학")
+                .build();
+        Student s2 = Student.builder()
+                .name("춘식이")
+                .city("서울시")
+                .major("컴퓨터공학")
+                .build();
+        Student s3 = Student.builder()
+                .name("어피치")
+                .city("제주도")
+                .major("화학공학")
+                .build();
+        studentRepository.save(s1);
+        studentRepository.save(s2);
+        studentRepository.save(s3);
+    }
+
+    @Test
+    @DisplayName("이름이 춘식이인 학생의 모든 정보를 조회한다.")
+    void findByNameTest() {
+        // given
+        String name = "춘식이";
+
+
+        // when
+        List<Student> list = studentRepository.findByName(name);
+
+        // then
+        assertEquals(1, list.size());
+        System.out.println("\n\n\n\n\n");
+        System.out.println(list.get(0));
+    }
+
+    @Test
+    @DisplayName("여러가지 조건을 쿼리메서드로 검색")
+    void queryMethodTest() {
+        // given
+        String city = "제주도";
+        String major = "공학";
+
+        // when
+
+        List<Student> students = studentRepository.findByMajorEndingWith(major);
+
+        // then
+
+        System.out.println("\n\n\n\n\n");
+        System.out.println(students.get(0));
+    }
 }
